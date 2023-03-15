385836f6eba100c77f8d7923a561600115ee8378
ryanfodera <ryanfodera@pursuit.org>
2023-03-15 14:51:27 -0400

    rf-work

--- src/Components/SoloVideo.js
+++ src/Components/SoloVideo.js
@@ -2,39 +2,30 @@
 // ! The individual video view.
 // ! This video view should, at minimum, have a playable video from YouTube.
 
-// ? fetching the selected video via users search; using api key w/i fecth info
-// ? make a state for search & user search 
-
-
-import React, {useEffect,useState} from 'react'
+import React, { useEffect } from 'react'
 import Youtube from "react-youtube"
-import {useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
-
-export default function SoloVideo({solovid}) {
+export default function SoloVideo() {
     console.log("test")
-    const {id} = useParams()
-    const [soloVideo, setSoloVideo] = useState([])
-    
+    const { id } = useParams()
+
+    useEffect(() => {
+        window.localStorage.setItem(id, JSON.stringify())
+    }, [id])
 
 
-    // const URL = process.env.REACT_APP_YOUTUBE_API_KEY
-    // const { id } = useParams() // -> to see if you can pull that video id from the 
-    // useEffect(() => {
-    //     fetch(`https://www.googleapis.com/youtube/v3/videos?id=${search}&key=${URL}&part=snippet,player`)
-    //         .then((response) => response.json())
-    //         .then((response) => {
-    //             // setSearchVid(searchVid)
-    //             console.log("test")
+    return (
+        <div className="clicked-vid">
+            <h3>Selected Video</h3>
 
-    //         })
-    //         .catch((error) => {
-    //             console.log(error)
-    //         }, [])
-    // })
+            <form >
+                <Youtube videoId={id} />
+            </form>
 
- return null;
-   
+        </div>
+    )
+
 
 }
 
